b52e098b76567c5362dd25bf0aae449ba4d47368
kendrick chukwuka <kendrickchibueze@gmail.com>
2023-02-21 17:06:30 +0100

    first commit

---
+++ app/src/main/java/com/example/miwok/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.miwok;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+// import android.support.design.widget.TabLayout;
+// import android.support.v4.view.ViewPager;
+// import android.support.v7.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // StrictMode is a developer tool which detects things you might be doing by accident and brings them to your attention so you can fix them.
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .build());
+        StrictMode.enableDefaults();
+
+        super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+
+        // Find the tab layout that shows the tabs
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        // Connect the tab layout with the view pager. This will
+        //   1. Update the tab layout when the view pager is swiped
+        //   2. Update the view pager when a tab is selected
+        //   3. Set the tab layout's tab names with the view pager's adapter's titles
+        //      by calling onPageTitle()
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
